11936323eb0ad575e5cf801c23eecc07989684f3
nikkisora <nikkisora.dev@gmail.com>
2022-02-27 16:11:55 +0300

    tidy up

--- l1/common.py
+++ l1/common.py
@@ -25,10 +25,41 @@ def lev_d(str1, str2):
             distance[row][col] = min(distance[row-1][col] + 1,
                                      distance[row][col-1] + 1,
                                      distance[row-1][col-1] + cost)
-
+    print(distance)
     ratio = 1 - distance[row][col] / max(len(str1), len(str2))
     return round(ratio, 5), distance[row][col]
 
+def d_lev_d(str1, str2):
+    """Return Demerau-Levenshtein distance between two strings
+    Returns:
+        tuple: (normalized distance, distance in steps)
+    """
+    # Initialize starting matrix
+    rows = len(str1)+1
+    cols = len(str2)+1
+    distance = np.zeros((rows, cols), dtype=int)
+
+    for row in range(1, rows):
+        distance[row][0] = row
+    for col in range(1, cols):
+        distance[0][col] = col
+
+    # Calculate distance matrix
+    for col in range(1, cols):
+        for row in range(1, rows):
+            cost = str1[row-1] != str2[col-1]
+            distance[row][col] = min(distance[row-1][col] + 1,
+                                     distance[row][col-1] + 1,
+                                     distance[row-1][col-1] + cost)
+            # Check for transposition
+            if (col > 1 and row > 1 and
+                str1[row-1] == str2[col-2] and
+                str1[row-2] == str2[col-1]):
+                distance[row][col] = min(distance[row][col],
+                                         distance[row-2][col-2] + 1)
+
+    ratio = 1 - distance[row][col] / max(len(str1), len(str2))
+    return round(ratio, 5), distance[row][col]
 
 def jac_d(str1, str2):
     """Return Jaccard distance between two strings"""
--- l1/spellcheck.py
+++ l1/spellcheck.py
@@ -4,7 +4,7 @@ import json
 
 import pandas as pd
 
-from common import lev_d, jac_d
+from common import d_lev_d, jac_d
 
 
 def spell(str1, dic, ret=5, dist=0.8, steps=0):
@@ -14,8 +14,10 @@ def spell(str1, dic, ret=5, dist=0.8, steps=0):
         str1 (str): input string
         dic (dict): Dictionary of words created by generate_dictionary.py
         ret (int, optional): Number of rows in the return table. Defaults to 5.
-        dist (float, optional): Normalized allowed distance between hashes computed by Jaccard distance. Defaults to 0.8.
-        steps (int, optional): Allowed distance in number of different characters in an input string. Defaults to 0.
+        dist (float, optional): Normalized allowed distance between hashes
+                                computed by Jaccard distance. Defaults to 0.8.
+        steps (int, optional): Allowed distance in number of different characters
+                               in an input string. Defaults to 0.
 
     Returns:
         DataFrame: A table containing two rows word and distance sorted by distance
@@ -29,9 +31,8 @@ def spell(str1, dic, ret=5, dist=0.8, steps=0):
         if jac_d(key, str1) > dist:
             for word in dic[key]:
                 table['word'].append(word)
-                table['distance'].append(lev_d(str1, word))
-    df = pd.DataFrame(table)
-    return df.sort_values('distance', ascending=False).head(ret)
+                table['distance'].append(d_lev_d(str1, word))
+    return pd.DataFrame(table).sort_values('distance', ascending=False).head(ret)
 
 def spell2(str1, dic, ret=5, dist=0.8, steps=0, freq_imp = 0.2):
     """Return a table of words sorted by distance to the input string
@@ -40,8 +41,10 @@ def spell2(str1, dic, ret=5, dist=0.8, steps=0, freq_imp = 0.2):
         str1 (str): input string
         dic (dict): Dictionary of words created by generate_dictionary.py
         ret (int, optional): Number of rows in the return table. Defaults to 5.
-        dist (float, optional): Normalized allowed distance between hashes computed by Jaccard distance. Defaults to 0.8.
-        steps (int, optional): Allowed distance in number of different characters in an input string. Defaults to 0.
+        dist (float, optional): Normalized allowed distance between hashes
+                                computed by Jaccard distance. Defaults to 0.8.
+        steps (int, optional): Allowed distance in number of different characters
+                               in an input string. Defaults to 0.
         freq_imp (float, optional): How much word frequency affects distance. Defaults to 0.2.
 
     Returns:
@@ -56,14 +59,13 @@ def spell2(str1, dic, ret=5, dist=0.8, steps=0, freq_imp = 0.2):
         if jac_d(key, str1) > dist:
             for word in dic[key]:
                 table['word'].append(word[0])
-                table['distance'].append((1-freq_imp)*lev_d(str1, word[0])[0]
+                table['distance'].append((1-freq_imp)*d_lev_d(str1, word[0])[0]
                                          +freq_imp*word[1])
-    df = pd.DataFrame(table)
-    return df.sort_values('distance', ascending=False).head(ret)
+    return pd.DataFrame(table).sort_values('distance', ascending=False).head(ret)
 
 
 if __name__ == '__main__':
-    with open('l1/dictionaries/words_freq_dict.json', 'r') as json_file:
+    with open('l1/dictionaries/words_freq_dict.json', 'r', encoding='utf-8') as json_file:
         word_dict = json.load(json_file)
     while 1:
         try:
@@ -71,3 +73,12 @@ if __name__ == '__main__':
             print(spell2(inp, word_dict, steps=2, freq_imp=0.5))
         except KeyboardInterrupt:
             break
+
+    # with open('l1/dictionaries/words_rand.json', 'r', encoding='utf-8') as json_file:
+    #     word_dict = json.load(json_file)
+    # while 1:
+    #     try:
+    #         inp = input()
+    #         print(spell(inp, word_dict, steps=2))
+    #     except KeyboardInterrupt:
+    #         break
